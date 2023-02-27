6037342914b75277dbce335224f2119ffa905ffb
Trevor Hattendorf <Trevor.hattendorf@gmail.com>
2023-02-27 11:24:01 -0600

    Add project files.

---
+++ AutoMotive/Form1.Designer.vb
@@ -0,0 +1,476 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lubeJob = New System.Windows.Forms.CheckBox()
+        Me.oilChange = New System.Windows.Forms.CheckBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.transFlush = New System.Windows.Forms.CheckBox()
+        Me.radiatorFlush = New System.Windows.Forms.CheckBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.tireRotation = New System.Windows.Forms.CheckBox()
+        Me.mufflerReplace = New System.Windows.Forms.CheckBox()
+        Me.inspection = New System.Windows.Forms.CheckBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.laborTextBox = New System.Windows.Forms.TextBox()
+        Me.partsTextBox = New System.Windows.Forms.TextBox()
+        Me.taxOnPartsLabel = New System.Windows.Forms.Label()
+        Me.totalFees = New System.Windows.Forms.Label()
+        Me.partsLabel = New System.Windows.Forms.Label()
+        Me.serviceLabel = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.totalFeesLabel = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox6 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        Me.GroupBox6.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(158, 397)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 41)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(322, 397)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 41)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(501, 397)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(92, 41)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(174, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 15)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "28.00"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(174, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 15)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "36.00"
+        '
+        'lubeJob
+        '
+        Me.lubeJob.AutoSize = True
+        Me.lubeJob.Location = New System.Drawing.Point(6, 62)
+        Me.lubeJob.Name = "lubeJob"
+        Me.lubeJob.Size = New System.Drawing.Size(73, 19)
+        Me.lubeJob.TabIndex = 6
+        Me.lubeJob.Text = "Lube Job"
+        Me.lubeJob.UseVisualStyleBackColor = True
+        '
+        'oilChange
+        '
+        Me.oilChange.AutoSize = True
+        Me.oilChange.Location = New System.Drawing.Point(6, 23)
+        Me.oilChange.Name = "oilChange"
+        Me.oilChange.Size = New System.Drawing.Size(85, 19)
+        Me.oilChange.TabIndex = 5
+        Me.oilChange.Text = "Oil Change"
+        Me.oilChange.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(204, 70)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(40, 15)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "120.00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(204, 31)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 15)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "50.00"
+        '
+        'transFlush
+        '
+        Me.transFlush.AutoSize = True
+        Me.transFlush.Location = New System.Drawing.Point(17, 66)
+        Me.transFlush.Name = "transFlush"
+        Me.transFlush.Size = New System.Drawing.Size(125, 19)
+        Me.transFlush.TabIndex = 6
+        Me.transFlush.Text = "Transmission Flush"
+        Me.transFlush.UseVisualStyleBackColor = True
+        '
+        'radiatorFlush
+        '
+        Me.radiatorFlush.AutoSize = True
+        Me.radiatorFlush.Location = New System.Drawing.Point(17, 27)
+        Me.radiatorFlush.Name = "radiatorFlush"
+        Me.radiatorFlush.Size = New System.Drawing.Size(101, 19)
+        Me.radiatorFlush.TabIndex = 5
+        Me.radiatorFlush.Text = "Radiator Flush"
+        Me.radiatorFlush.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(198, 86)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(34, 15)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "20.00"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(198, 61)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 15)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "200.00"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(198, 32)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(34, 15)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "15.00"
+        '
+        'tireRotation
+        '
+        Me.tireRotation.AutoSize = True
+        Me.tireRotation.Location = New System.Drawing.Point(9, 82)
+        Me.tireRotation.Name = "tireRotation"
+        Me.tireRotation.Size = New System.Drawing.Size(93, 19)
+        Me.tireRotation.TabIndex = 7
+        Me.tireRotation.Text = "Tire Rotation"
+        Me.tireRotation.UseVisualStyleBackColor = True
+        '
+        'mufflerReplace
+        '
+        Me.mufflerReplace.AutoSize = True
+        Me.mufflerReplace.Location = New System.Drawing.Point(9, 57)
+        Me.mufflerReplace.Name = "mufflerReplace"
+        Me.mufflerReplace.Size = New System.Drawing.Size(109, 19)
+        Me.mufflerReplace.TabIndex = 6
+        Me.mufflerReplace.Text = "Replace Muffler"
+        Me.mufflerReplace.UseVisualStyleBackColor = True
+        '
+        'inspection
+        '
+        Me.inspection.AutoSize = True
+        Me.inspection.Location = New System.Drawing.Point(9, 32)
+        Me.inspection.Name = "inspection"
+        Me.inspection.Size = New System.Drawing.Size(81, 19)
+        Me.inspection.TabIndex = 5
+        Me.inspection.Text = "Inspection"
+        Me.inspection.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(9, 66)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(37, 15)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Labor"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(9, 22)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(36, 15)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Parts "
+        '
+        'laborTextBox
+        '
+        Me.laborTextBox.Location = New System.Drawing.Point(72, 66)
+        Me.laborTextBox.Name = "laborTextBox"
+        Me.laborTextBox.Size = New System.Drawing.Size(67, 23)
+        Me.laborTextBox.TabIndex = 1
+        '
+        'partsTextBox
+        '
+        Me.partsTextBox.Location = New System.Drawing.Point(72, 22)
+        Me.partsTextBox.Name = "partsTextBox"
+        Me.partsTextBox.Size = New System.Drawing.Size(67, 23)
+        Me.partsTextBox.TabIndex = 0
+        '
+        'taxOnPartsLabel
+        '
+        Me.taxOnPartsLabel.AutoSize = True
+        Me.taxOnPartsLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.taxOnPartsLabel.Location = New System.Drawing.Point(505, 31)
+        Me.taxOnPartsLabel.Name = "taxOnPartsLabel"
+        Me.taxOnPartsLabel.Size = New System.Drawing.Size(2, 17)
+        Me.taxOnPartsLabel.TabIndex = 18
+        '
+        'totalFees
+        '
+        Me.totalFees.AutoSize = True
+        Me.totalFees.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.totalFees.Location = New System.Drawing.Point(505, 68)
+        Me.totalFees.Name = "totalFees"
+        Me.totalFees.Size = New System.Drawing.Size(2, 17)
+        Me.totalFees.TabIndex = 17
+        '
+        'partsLabel
+        '
+        Me.partsLabel.AutoSize = True
+        Me.partsLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.partsLabel.Location = New System.Drawing.Point(189, 64)
+        Me.partsLabel.Name = "partsLabel"
+        Me.partsLabel.Size = New System.Drawing.Size(2, 17)
+        Me.partsLabel.TabIndex = 16
+        '
+        'serviceLabel
+        '
+        Me.serviceLabel.AutoSize = True
+        Me.serviceLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.serviceLabel.Location = New System.Drawing.Point(189, 27)
+        Me.serviceLabel.Name = "serviceLabel"
+        Me.serviceLabel.Size = New System.Drawing.Size(2, 17)
+        Me.serviceLabel.TabIndex = 15
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.Location = New System.Drawing.Point(302, 31)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(75, 15)
+        Me.Label19.TabIndex = 14
+        Me.Label19.Text = "Tax(on parts)"
+        '
+        'totalFeesLabel
+        '
+        Me.totalFeesLabel.AutoSize = True
+        Me.totalFeesLabel.Location = New System.Drawing.Point(317, 68)
+        Me.totalFeesLabel.Name = "totalFeesLabel"
+        Me.totalFeesLabel.Size = New System.Drawing.Size(58, 15)
+        Me.totalFeesLabel.TabIndex = 13
+        Me.totalFeesLabel.Text = "Total Fees"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(54, 66)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(33, 15)
+        Me.Label15.TabIndex = 10
+        Me.Label15.Text = "Parts"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(0, 29)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(100, 15)
+        Me.Label16.TabIndex = 11
+        Me.Label16.Text = "Service and Labor"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.GroupBox4)
+        Me.GroupBox1.Controls.Add(Me.oilChange)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.lubeJob)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(107, 27)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(269, 100)
+        Me.GroupBox1.TabIndex = 18
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Oil and Lubrication"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.radiatorFlush)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.transFlush)
+        Me.GroupBox2.Location = New System.Drawing.Point(420, 27)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(267, 100)
+        Me.GroupBox2.TabIndex = 19
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Radiator and Transmission"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.mufflerReplace)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.inspection)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Controls.Add(Me.tireRotation)
+        Me.GroupBox3.Location = New System.Drawing.Point(104, 150)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(272, 112)
+        Me.GroupBox3.TabIndex = 20
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Miscellaneous"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Location = New System.Drawing.Point(134, 99)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox4.TabIndex = 21
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "GroupBox4"
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.Label9)
+        Me.GroupBox5.Controls.Add(Me.partsTextBox)
+        Me.GroupBox5.Controls.Add(Me.Label8)
+        Me.GroupBox5.Controls.Add(Me.laborTextBox)
+        Me.GroupBox5.Location = New System.Drawing.Point(435, 162)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(252, 100)
+        Me.GroupBox5.TabIndex = 13
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Parts and Labor"
+        '
+        'GroupBox6
+        '
+        Me.GroupBox6.Controls.Add(Me.taxOnPartsLabel)
+        Me.GroupBox6.Controls.Add(Me.Label16)
+        Me.GroupBox6.Controls.Add(Me.totalFees)
+        Me.GroupBox6.Controls.Add(Me.Label15)
+        Me.GroupBox6.Controls.Add(Me.partsLabel)
+        Me.GroupBox6.Controls.Add(Me.totalFeesLabel)
+        Me.GroupBox6.Controls.Add(Me.serviceLabel)
+        Me.GroupBox6.Controls.Add(Me.Label19)
+        Me.GroupBox6.Location = New System.Drawing.Point(104, 291)
+        Me.GroupBox6.Name = "GroupBox6"
+        Me.GroupBox6.Size = New System.Drawing.Size(583, 100)
+        Me.GroupBox6.TabIndex = 21
+        Me.GroupBox6.TabStop = False
+        Me.GroupBox6.Text = "Summary Of Charges"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox6)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "TG Automotive"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox5.PerformLayout()
+        Me.GroupBox6.ResumeLayout(False)
+        Me.GroupBox6.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lubeJob As CheckBox
+    Friend WithEvents oilChange As CheckBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents transFlush As CheckBox
+    Friend WithEvents radiatorFlush As CheckBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents tireRotation As CheckBox
+    Friend WithEvents mufflerReplace As CheckBox
+    Friend WithEvents inspection As CheckBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents laborTextBox As TextBox
+    Friend WithEvents partsTextBox As TextBox
+    Friend WithEvents taxOnPartsLabel As Label
+    Friend WithEvents totalFees As Label
+    Friend WithEvents partsLabel As Label
+    Friend WithEvents serviceLabel As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents totalFeesLabel As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents GroupBox6 As GroupBox
+End Class
---
+++ AutoMotive/Form1.vb
@@ -0,0 +1,79 @@
+﻿Public Class Form1
+    Const dblTaxRate As Decimal = 0.06
+    Const dblOil As Double = 36
+    Const dblLubeJob As Double = 28
+    Const dblRadiatorFlush As Double = 50
+    Const dblTransFlush As Double = 120
+    Const dblMuffler As Double = 200
+    Const dblInspection As Double = 15
+    Const dblTireRotation As Double = 20
+
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim taxes As Decimal
+        Dim subTotal
+        Dim totalFees As Decimal
+
+        subTotal = dblOil + dblLubeJob + dblRadiatorFlush + dblTransFlush + dblInspection + dblTireRotation + dblMuffler
+        taxes = CalcTax()
+        totalFees = subTotal + taxes
+        taxOnPartsLabel.Text = taxes.ToString("c")
+        totalFeesLabel.Text = totalFees.ToString("c")
+
+
+
+    End Sub
+
+    Function OilandLube() As Decimal
+        Dim decOil As Decimal
+        If oilChange.Checked = True Then
+            dblOil += oilChange()
+        Else
+            dblLubeJob = lubeJob()
+        End If
+    End Function
+
+    Function RadiatorandTrans() As Decimal
+        If radiatorFlush.Checked = True Then
+            dblRadiatorFlush += radiatorFlush()
+        Else
+            dblTransFlush = transFlush()
+        End If
+    End Function
+
+    Function Miscallenous()
+        If inspection.Checked = True Then
+            inspection += dblInspection()
+        ElseIf mufflerReplace.Checked = True Then
+            mufflerReplace += dblMuffler()
+        Else
+            tireRotation.Checked = True
+            tireRotation += dblTireRotation
+        End If
+    End Function
+
+    Function CalcTax(ByVal decAmount As Decimal) As Decimal
+        Return decAmount * dblTaxRate
+    End Function
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        oilChange.Checked = False
+        lubeJob.Checked = False
+        radiatorFlush.Checked = False
+        transFlush.Checked = False
+        inspection.Checked = False
+        mufflerReplace.Checked = False
+        tireRotation.Checked = False
+        partsTextBox.Text = ""
+        laborTextBox.Text = ""
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+End Class
